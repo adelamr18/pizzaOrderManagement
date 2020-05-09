f4e2e700d1c2416dfc044d1dc965914afc5ab1a5
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-09 16:24:43 +0200

    Move all static text to shared defines file

---
+++ client/src/shared/constants/defines.tsx
@@ -0,0 +1,38 @@
+export const titles = {
+  login: "Login",
+  popularDishes: "Popular dishes",
+  shoppingBasket: "Basket",
+  shoppingCartText: "Add some tasty food from the menu and place your order.",
+  subtotal: "Subtotal",
+  delivery: "Delivery costs",
+  checkoutHeader: "Here we go!",
+  checkoutRestaurant: "Pizza Thomas Nuremberg",
+  checkoutSubheader: "Where should your order be delivered to?",
+  confirmOrder: "Confirm order",
+  restaurantName: "Pizza Thomas",
+  euros: "€",
+  dollars: "$",
+  order: "order"
+};
+
+export const formlLabels = {
+  firstname: "First Name",
+  surname: "Surname",
+  address: "Address",
+  postalCode: "Zip",
+  state: "State",
+  city: "City"
+};
+
+export const notifications = {
+  errors: {
+    firstName: "Firstname is a required field",
+    surname: "Surname is a required field",
+    city: "City is a required field",
+    address: "Address is a required field",
+    state: "State is a required field",
+    zip: "Zip is a required field",
+    emptyCart: "You must select an order to proceed to checkout."
+  },
+  successToast: "You have confirmed your order !"
+};
